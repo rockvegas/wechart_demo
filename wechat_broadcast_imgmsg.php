e933bb582d16d957f904e04d1df9fad3590deae8
rockvegas <rockvegas@aliyun.com>
2016-07-18 22:52:55 +0800

    add access token realtime support

--- wechat_broadcast_imgmsg.php
+++ wechat_broadcast_imgmsg.php
@@ -1,10 +1,23 @@
 <?php
+//更换成自己的APPID和APPSECRET            
+$TOKEN_URL="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx1156b8ab632c2f99&secret=adffa603ee1cf4e0a37c41ff5bd28502";
 
-$accessToken = "M-9OXPZlt4nWtr4yuy8lYtSR9t5ohh7as4mHFIp2Lz_w1Lsqr9iCA83Vt9gyhBomIAiRyQiJdV9c6b0v-ZIpmYYUUh4Qj7iD9vArqkwCJGLHhgcnK_CgIRa9Ubp4WUBRBNKaAHARDA";
+$objCurl = curl_init($TOKEN_URL);
+curl_setopt($objCurl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($objCurl, CURLOPT_TIMEOUT, 3);
+$json = curl_exec($objCurl);
+
+$result=json_decode($json, true);
+//print_r($result);
+
+//var_dump($result);
+$accToken=$result['access_token'];
+
+$accToken = "M-9OXPZlt4nWtr4yuy8lYtSR9t5ohh7as4mHFIp2Lz_w1Lsqr9iCA83Vt9gyhBomIAiRyQiJdV9c6b0v-ZIpmYYUUh4Qj7iD9vArqkwCJGLHhgcnK_CgIRa9Ubp4WUBRBNKaAHARDA";
 $imgUri = "http://mmbiz.qpic.cn/mmbiz/M2mv954iaHeku9iaKl0djmv8LHjMsjpPmltxM1BR4c9bslxrbgc2ibcMm4qo83tczx6qaicIGLLbdI5mdPGEuAf6lg/0";
 $imgID = "p4_BtL0HLohIZbne0RygSPbJwskErSHwIazWhDjK5K7YbQUNWbKCaYiJx1EAykeo";
 
-$uploadUrl = "https://api.weixin.qq.com/cgi-bin/media/uploadnews?access_token=".$accessToken;
+$uploadUrl = "https://api.weixin.qq.com/cgi-bin/media/uploadnews?access_token=".$accToken;
 // $rawData = array('touser' => $toUser,
 //               'msgtype' => 'text',
 //               'text' => array('content' => $msg));
@@ -34,5 +47,8 @@ if (curl_errno($curl)) {
 curl_close($curl);
 
 var_dump($result);
+//artical id:  4g4FLSc5J8BfzJj3Eg41rvI5d28bekX4P0dOnHrTeWPYP3e7oWR4udKhzRGPUdvp
+
+//TODO: send broad cast img msg...
 
 ?>
